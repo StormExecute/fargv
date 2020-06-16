f4456f5e41af985c7730ac2d62666f24338e87e1
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-17 02:45:19 +0300

    git: add cross-platform default patches

---
+++ tests/_paths.js
@@ -0,0 +1,24 @@
+const windows = {
+	
+	execFilePath: 'D:\\NodeProjects\\fargv\\tests\\main.js',
+	execNodePath: 'C:\\Program Files\\nodejs\\node.exe'
+	
+};
+
+const linux = {
+	
+	execFilePath: '',
+	execNodePath: ''
+	
+};
+
+if(process.platform.startsWith("win")) {
+
+	module.exports.execFilePath = windows.execFilePath;
+	module.exports.execNodePath = windows.execNodePath;
+
+} else {
+	
+	throw new Error("Tests for other OS are temporarily not supported.");
+	
+}
