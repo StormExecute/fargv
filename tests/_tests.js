da303e3f7e2a97e24b29d289e48ebcea7fb1fb00
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-17 02:55:13 +0300

    git: add option test file

---
+++ tests/_tests.js
@@ -0,0 +1,53 @@
+module.exports = [
+
+	{
+		
+		/* allTypes: true,
+		
+		objectParse: {
+			
+			allTypes: true,
+			
+		}, */
+		
+	},
+	
+	{
+		
+		noParse: true,
+		
+		rememberExecFilePath: false,
+		
+		rememberExecNodePath: true,
+		
+	},
+	
+	{
+		
+		rememberExecFilePath: false,
+		
+		supportEmptyFlags: false,
+		
+	},
+
+	{
+		
+		supportOnlyLatinArgs: false,
+		
+		rememberExecNodePath: true,
+		
+		mainTypes: true,
+		
+		arrayParse: {
+			
+			bigint: true,
+			
+			defaultNoneValue: 444,
+			
+		},
+		
+		defaultNoneValue: 123,
+		
+	},
+
+];
