30bd3f1c09cc8b4932318e2ed286708de387e053
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-17 02:51:46 +0300

    git: add main test file

---
+++ tests/main.js
@@ -0,0 +1,69 @@
+/*
+
+	node tests/main.js --someNumber=1 -a="[1, 2, 4n, [1, 2, 3, [string, [string and string, and others, []], yeah, true], 9, .], test test, NaN,,]" -b=22n ---099=123 --none= --noneWithoutEqSym -object="{someProp: 123, noneDefault: , array: [yeah, it, s, supported!]}"
+	
+*/
+
+const fargv = require("../src/main");
+
+const tests = require("./_tests");
+const results = require("./_results");
+
+const objectEquals = require("../dependencies/objectEquals");
+const objectDifference = require("../dependencies/objectDiff");
+const toJson = require("../dependencies/toJson");
+
+function test(id, test, result) {
+	
+	if(objectEquals(test, result)) {
+		
+		console.log(`Test #${id}: OK`);
+		
+		//console.log(test);
+		
+	} else {
+		
+		console.log(`Test #${id}: NO`);
+		
+		console.log("result of this test:", test);
+		
+		//console.log(toJson(test));
+		
+		console.log("proper result:", result);
+		
+		console.log("diff:", objectDifference(test, result));
+		
+	}
+	
+}
+
+
+for(let i = 0; i < tests.length; i++) {
+	
+	if(i % 2 == 0) {
+	
+		test(
+		
+			i + 1, 
+		
+			fargv(tests[i]), 
+			
+			results[i]
+			
+		);
+		
+	} else {
+		
+		test(
+		
+			i + 1, 
+			
+			fargv.options(tests[i]).init(),
+			
+			results[i]
+			
+		);
+		
+	}
+	
+}
