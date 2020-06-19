ef115d73a9d1757f98ceba844dea40898eb2fbaa
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-19 19:41:57 +0300

    add: merging

--- src/static/setOptions.js
+++ src/static/setOptions.js
@@ -1,6 +1,18 @@
-const staticSetOptions = function(options) {
+const isObject = require("../../dependencies/isObject");
+
+const staticSetOptions = function(options, merging) {
 	
-	this._options = options;
+	if(!merging) {
+		
+		this._options = options;
+		
+	} else {
+		
+		if(!isObject(this._options)) this._options = {};
+		
+		this._options = Object.assign(this._options, options);
+		
+	}
 	
 	return this;
 	
