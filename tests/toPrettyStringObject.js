4cbbe7abcb7f341649af0ba58c03dcf53c692ff9
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-22 16:40:49 +0300

    git: add custom pretty writer objects

---
+++ tests/toPrettyStringObject.js
@@ -0,0 +1,48 @@
+const isObject = require("../dependencies/isObject");
+const isNumeric = require("../dependencies/isNumeric");
+
+const { fromArray, fromObject } = require("../src/main");
+
+const toPrettyStringObject = (sourceObject, useQuotesForProps) => {
+	
+	let result = "{";
+	
+	useQuotesForProps = useQuotesForProps ? '"' : '';
+	
+	for(const objectProp in sourceObject) {
+		
+		let objectValue = sourceObject[objectProp];
+		
+		let useQuotesForValues = '"';
+		
+		if(isObject(objectValue)) {
+			
+			objectValue = fromObject(objectValue, true);
+			
+		} else if(Array.isArray(objectValue)) {
+			
+			objectValue = fromArray(objectValue, true);
+			
+		} else if(typeof objectValue == "bigint") {
+			
+			objectValue = objectValue + "n";
+			
+		} else if(isNumeric(objectValue) || objectValue == true || objectValue == false || objectValue == null || objectValue == undefined || objectValue == NaN || objectValue == Infinity) {
+			
+			useQuotesForValues = '';
+			
+		}
+		
+		result += `\n\n\t${useQuotesForProps}${objectProp}${useQuotesForProps}: ${useQuotesForValues}${objectValue}${useQuotesForValues},`;
+		
+	}
+	
+	if(result == "{") return "{}";
+	
+	result += "\n\n}";
+	
+	return result;
+	
+};
+
+module.exports = toPrettyStringObject;
