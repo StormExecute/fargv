5946f01c75f8a848832ef8c46b1c0181467cffee
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-22 04:08:10 +0300

    add: stable error data with codes

---
+++ src/data/_errors.js
@@ -0,0 +1,17 @@
+module.exports = {
+	
+	0: "None",
+	
+	1: "Maximum call stack size exceeded appears!",
+	
+	200: "Can't parse array in array.",
+	201: "Can't parse object in array.",
+	
+	300: "Can't parse object in object property.",
+	301: "Can't parse array in object property.",
+	
+	302: "Object doesn't have a colon.",
+	303: "Parsing stuck on possible object prop value.",
+	304: "Duplicate object keys detected.",
+	
+};
