4a705d2626aa136633023f029466822a6cca0649
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-23 22:36:48 +0300

    git: add demand test

---
+++ tests/demandArgs.js
@@ -0,0 +1,19 @@
+const fargv = require("../src/main");
+
+fargv.demand("someNumber");
+
+fargv.demand(["a", "b"]);
+
+fargv.demand("???");
+
+fargv.undemand("???");
+
+fargv.demand(["noneExistsArg", "and his friend"]);
+
+fargv.undemand(["noneExistsArg", "and his friend"]);
+
+fargv.demand(["099"], true);
+
+fargv();
+
+console.log("OK.");
