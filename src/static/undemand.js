35b7c2dcd5acd4b5388c91419ad307e7d9dce336
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-23 22:34:29 +0300

    git: add fargv.undemand core

---
+++ src/static/undemand.js
@@ -0,0 +1,25 @@
+const isObject = require("../../dependencies/isObject");
+
+const staticUnDemand = function(args, withSkipArgs) {
+	
+	if(!isObject(this._options)) return this;
+	
+	const demandType = withSkipArgs ? "demandWithSkipArgs" : "demandArgs";
+	
+	if(!Array.isArray(this._options[demandType])) return this;
+	
+	args = Array.isArray(args) ? args : [args];
+	
+	for(let i = 0; i < args.length; i++) {
+		
+		const indexOf = this._options[demandType].indexOf(args[i]);
+		
+		if(indexOf != -1) this._options[demandType].splice(indexOf, 1);
+		
+	}
+	
+	return this;
+	
+};
+
+module.exports = staticUnDemand;
