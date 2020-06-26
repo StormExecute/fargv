8b03058349f042b3185337df7160d17764f0eeb6
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-26 22:18:32 +0300

    add: mergingWrapOptions to fargv.init(nowOptions, mergingWithWrapper)

--- src/static/emulateInit.js
+++ src/static/emulateInit.js
@@ -1,6 +1,14 @@
-const init = function(options) {
+const isObject = require("../../dependencies/isObject");
+
+const { deepCloneObject } = require("../../dependencies/deepClone");
+
+const init = function(options, mergingWithFargvWrapperOptions) {
 	
-	return this(options);
+	if(!mergingWithFargvWrapperOptions) return this(options);
+	
+	if(!isObject(options)) return this();
+	
+	return this(deepCloneObject({}, this._options, options));
 	
 };
 
