556b7bacb075ec74fa4eb50dff60531ab39795b2
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-26 22:19:36 +0300

    git: add fargv.default core

--- src/static/setDefault.js
+++ src/static/setDefault.js
@@ -1,9 +1,79 @@
-const staticSetDefault = function(_default) {
+const isObject = require("../../dependencies/isObject");
+
+const copyV = require("../../dependencies/copyValWithoutBind");
+
+const toJson = require("../../dependencies/toJson");
+
+const getValue = require("../../dependencies/getAnyPropValIfExists");
+
+/*
+
+Format:
+
+	{
 	
-	this._default = _default;
+		...[name]: value | {
+			
+			value | argValue | v = any
+			
+			alias | a = string | array
+			
+		}
+	
+	}
+	
+*/
+
+const staticSetDefaultArgValues = function(objectOfValues) {
+	
+	if(!isObject(objectOfValues) || toJson(objectOfValues) == "{}") return this;
+	
+	const defaultArgv = {};
+	
+	for(const argName in objectOfValues) {
+		
+		let argValue = copyV(objectOfValues[argName]);
+		
+		if(isObject(argValue) && isObject(argValue._options)) {
+			
+			argValue = argValue._options;
+			
+			const value = getValue(argValue, ["value", "v", "argValue"]);
+			
+			const alias = getValue(argValue, ["alias", "a", "aliases"]);
+			
+			if(Array.isArray(alias)) {
+				
+				for(let i = 0; i < alias.length; i++) {
+					
+					defaultArgv[alias[i]] = value;
+					
+				}
+				
+			} else if(typeof alias == "string") {
+				
+				defaultArgv[alias] = value;
+				
+			}
+			
+			argValue = value;
+			
+		}
+		
+		defaultArgv[argName] = argValue;
+		
+	}
+	
+	this.options({
+	
+		defaultArgv,
+		
+		defaultNoneValue: undefined
+		
+	});
 	
 	return this;
 	
 };
 
-module.exports = staticSetDefault;
+module.exports = staticSetDefaultArgValues;
