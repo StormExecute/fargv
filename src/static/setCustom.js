26de6f0812948e80c59ce14dfca24a1bfa7c8921
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-28 04:15:23 +0300

    up: auto concat array elements

--- src/static/setCustom.js
+++ src/static/setCustom.js
@@ -2,6 +2,16 @@ const setCustomArgv = function(customArgv) {
 	
 	if(!Array.isArray(customArgv)) return this;
 	
+	for(let i = 0; i < customArgv.length; i++) {
+		
+		if(Array.isArray(customArgv[i]) && customArgv[i][0] && customArgv[i][0].startsWith("-") && typeof customArgv[i][1] == "string") {
+			
+			customArgv[i] = customArgv[i][0] + customArgv[i][1];
+			
+		}
+		
+	}
+	
 	this.options({
 		
 		customArgv
