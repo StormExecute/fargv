cabec7018a03ebf2e90d3a45cba4c1a11ca7e9f1
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-29 00:52:48 +0300

    add: concat options[typeNoParseFlags] && add: reset option

--- src/static/set/noParseFlags.js
+++ src/static/set/noParseFlags.js
@@ -1,14 +1,28 @@
+const isObject = require("../../../dependencies/isObject");
+
+const { deepCloneArray } = require("../../../dependencies/deepClone");
+
 const staticSetNoParseFlags = function(noParseFlags, isNoParseNoDefaultFlags) {
 	
 	if(!Array.isArray(noParseFlags) && typeof noParseFlags != "string") return this;
 	
-	noParseFlags = Array.isArray(noParseFlags) ? Object.assign([], noParseFlags) : [noParseFlags];
+	if(!isObject(this._options)) this._options = {};
 	
 	const typeNoParseFlags = isNoParseNoDefaultFlags ? "noParseNoDefaultFlags" : "noParseFlags";
 	
+	if(noParseFlags == "reset") {
+	
+		this._options[typeNoParseFlags] = null;
+		
+		return this;
+	
+	}
+	
+	noParseFlags = Array.isArray(noParseFlags) ? Object.assign([], noParseFlags) : [noParseFlags];
+	
 	return this.options({
 		
-		[typeNoParseFlags]: noParseFlags
+		[typeNoParseFlags]: Array.isArray(this._options[typeNoParseFlags]) ? deepCloneArray([], this._options[typeNoParseFlags], excludeFlags) : noParseFlags
 		
 	});
 	
