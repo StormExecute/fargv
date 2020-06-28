3c05d71f2cfcb87cd77fe0e6c8fd835ee4589a2d
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-28 16:44:04 +0300

    git: add customFlags test core

---
+++ tests/objects/customFlags.js
@@ -0,0 +1,60 @@
+const objectEquals = require("../../dependencies/objectEquals");
+const objectDifference = require("../../dependencies/objectDiff");
+
+const fargv = require("../../src/main");
+
+const requiredResult = {
+	
+	a: 24,
+	
+	a: 24,
+	
+	b: "72n",
+	
+	c: "hi",
+	
+	d: {hello: "world"},
+	
+	e: ["finish"],
+	
+	f: false,
+	
+	g: true,
+	
+};
+
+const testFlags = fargv.customWithGenerateFromObject({
+	
+	a: 24,
+	
+	b: 72n,
+	
+	c: "hi",
+	
+	d: {hello: "world"},
+	
+	e: ["finish"],
+	
+	f: undefined,
+	
+}).options({
+	
+	mainTypes: true
+	
+}).default({
+	
+	f: false,
+	
+	g: true,
+	
+}).init();
+
+if(objectEquals(testFlags, requiredResult)) {
+	
+	console.log("OK.");
+	
+} else {
+	
+	console.error(objectDifference(testFlags, requiredResult));
+	
+}
