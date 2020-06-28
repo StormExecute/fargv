0e154f1f6ddc703ed5a45d451977237fae7ed047
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-28 03:47:32 +0300

    add: new fargv.state core

--- src/static/getState.js
+++ src/static/getState.js
@@ -1,6 +1,54 @@
-const getStateOfOptionsAndDefaults = function() {
+const copyV = require("../../dependencies/copyValWithoutBind");
+
+const getStateOfOptionsAndDefaults = function(about) {
 	
-	return [this._options, this._default];
+	switch(about) {
+		
+		case "default":
+		
+			return copyV(this._options.defaultArgv);
+		
+		break;
+		
+		case "custom":
+		
+			return copyV(this._options.customArgv);
+		
+		break;
+		
+		case "demand": 
+		case "demandU": //demandUsually
+		
+			return copyV(this._options.demandFlags);
+		
+		break;
+		
+		case "demandWSF":
+		case "demandWithSkippedFlags":
+		case "demandWSFlags":
+		
+			return copyV(this._options.demandWithSkippedFlags);
+		
+		break;
+		
+		case "demandAll":
+		
+			return {
+				
+				withSkippedFlags: copyV(this._options.demandWithSkippedFlags),
+				flags: copyV(this._options.demandFlags)
+				
+			}
+		
+		break;
+		
+		default:
+		
+			return copyV(this._options) || {};
+		
+		break;
+		
+	}
 	
 };
 
