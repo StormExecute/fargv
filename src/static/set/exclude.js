d68d65fd23a04e263fd7583a13d553b7e2ea27cf
Ghost <vladimirvsevolodovi@yandex.ru>
2020-06-29 00:52:08 +0300

    add: concat options.excludeFlags && add: reset option

--- src/static/set/exclude.js
+++ src/static/set/exclude.js
@@ -1,12 +1,26 @@
+const isObject = require("../../../dependencies/isObject");
+
+const { deepCloneArray } = require("../../../dependencies/deepClone");
+
 const staticSetExcludeFlags = function(excludeFlags) {
 	
 	if(!Array.isArray(excludeFlags) && typeof excludeFlags != "string") return this;
+	
+	if(!isObject(this._options)) this._options = {};
+	
+	if(excludeFlags == "reset") {
+	
+		this._options.excludeFlags = null;
+		
+		return this;
+	
+	}
 	
 	excludeFlags = Array.isArray(excludeFlags) ? Object.assign([], excludeFlags) : [excludeFlags];
 	
 	return this.options({
 		
-		excludeFlags
+		excludeFlags: Array.isArray(this._options.excludeFlags) ? deepCloneArray([], this._options.excludeFlags, excludeFlags) : excludeFlags
 		
 	});
 	
