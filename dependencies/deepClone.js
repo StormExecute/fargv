befd8023f209496c21af09db90e3cc1a2243f011
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-01 18:18:00 +0300

    del: unused functional && up: performance

--- dependencies/deepClone.js
+++ dependencies/deepClone.js
@@ -1,28 +1,29 @@
 /*
-
-	deepClone
-	(c) 2020 Cameron Osakiski
-	deepClone(by Cameron Osakiski) may be freely distributed under the MIT license.
-	
-	dependencies:
-	
-		isObject from jQuery under the MIT license,
-		toJson from github
-
+ *
+ *	deepClone.js
+ *
+ *	Tools for cloning objects and arrays
+ *
+ *	(c) 2020-06-26 Cameron Osakiski
+ *
+ *	This script may be freely distributed under the MIT license.
+ *
+ *	Dependencies: isPlainObject, isEmptyObject
+ *
 */
 
 const isObject = require("./isObject");
-const toJson = require("./toJson");
+const isEmptyObject = require("./isEmptyObject");
 
-//only arrays
+//only arrays(with own objects)
 const deepCloneArray = function(sourceArray, ...arrays) {
 	
 	if(!Array.isArray(sourceArray)) return [false];
 	
-	const result = [].slice.call(sourceArray);
+	const result = !sourceArray.length ? [] : deepCloneArray([], sourceArray);
 	
-	for(let i = 0; i < arrays.length; i++) {
-		
+	for(let i = 0; i < arrays.length; ++i) {
+
 		const thisArray = arrays[i];
 		
 		if(!Array.isArray(thisArray)) continue;
@@ -42,39 +43,23 @@ const deepCloneArray = function(sourceArray, ...arrays) {
 	
 	return result;
 	
-}
+};
 
-//arrays + objects
+//only objects
 const deepCloneObject = function(mainObject, ...objects) {
 	
-	//only objects and arrays
-	if(!isObject(mainObject) && !Array.isArray(mainObject)) return {"false": false};
+	if(!isObject(mainObject)) return {"false": false};
 	
 	if(!objects.length) return mainObject;
 	
-	let result = toJson(mainObject) == "{}" ? {} 
-	: Array.isArray(mainObject) ? [].slice.call(mainObject)
-	: deepCloneObject.___spawnRecursionChild({}, mainObject);
+	let result = isEmptyObject(mainObject) ? {} : deepCloneObject({}, mainObject);
 	
-	for(let i = 0; i < objects.length; i++) {
+	for(let i = 0; i < objects.length; ++i) {
 		
 		const thisObject = objects[i];
 		
-		//skip none-objects && none-arrays
-		if(!isObject(thisObject) && !Array.isArray(thisObject)) continue;
-		
-		//if these are arrays return deepCloneArray
-		if(Array.isArray(result) && Array.isArray(thisObject)) {
-			
-			result = deepCloneArray(result, thisObject);
-			
-			continue;
-			
-		}
-		
-		//skip if second argument is of the wrong type
-		if(Array.isArray(result) && isObject(thisObject)) continue;
-		if(isObject(result) && Array.isArray(thisObject)) continue;
+		//skip none-objects
+		if(!isObject(thisObject)) continue;
 		
 		for(const prop in thisObject) {
 			
@@ -85,25 +70,17 @@ const deepCloneObject = function(mainObject, ...objects) {
 				
 				if(isObject(resultValue)) {
 					
-					result[prop] = deepCloneObject.___spawnRecursionChild(resultValue, thisValue);
+					result[prop] = deepCloneObject(resultValue, thisValue);
 					
 				} else {
 					
-					result[prop] = deepCloneObject.___spawnRecursionChild({}, thisValue);
+					result[prop] = deepCloneObject({}, thisValue);
 					
 				}
 				
 			} else if(Array.isArray(thisValue)) {
 				
-				if(Array.isArray(resultValue) && deepCloneObject._concatArrays.status) {
-					
-					result[prop] = deepCloneArray(resultValue, thisValue);
-					
-				} else {
-					
-					result[prop] = [].slice.call(thisValue);
-					
-				}
+				result[prop] = deepCloneArray([], thisValue);
 				
 			} else {
 				
@@ -115,58 +92,13 @@ const deepCloneObject = function(mainObject, ...objects) {
 		
 	}
 	
-	if(!deepCloneObject._concatArrays.always && deepCloneObject._concatArrays.status) {
-		
-		deepCloneObject._concatArrays.status = false;
-		
-	}
-	
 	return result;
 	
 };
-
-deepCloneObject._concatArrays = {};
-
-deepCloneObject.concatArrays = function(state) {
-	
-	if(!isObject(state)) {
-		
-		state = {
-			
-			status: state,
-			always: false
-			
-		};
-		
-	}
-	
-	deepCloneObject._concatArrays = {
-		
-		status: state.status || deepCloneObject._concatArrays.status || false,
-		always: state.always || deepCloneObject._concatArrays.always || false
-		
-	};
-	
-	return deepCloneObject
-	
-};
-
-//to save _concatArrays state for main process
-deepCloneObject.___spawnRecursionChild = function(mainObject, ...objects) {
-	
-	const _concatArraysState = {status: deepCloneObject._concatArrays.status, always: deepCloneObject._concatArrays.always};
-	
-	const result = deepCloneObject(mainObject, ...objects);
-	
-	deepCloneObject._concatArrays = {status: _concatArraysState.status, always: _concatArraysState.always};
-	
-	return result;
-	
-}
 
 module.exports = {
 	
 	deepCloneArray,
-	deepCloneObject
+	deepCloneObject,
 	
 };
