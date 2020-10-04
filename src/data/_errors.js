020581fd5da38b4bb0b0364c93eda9dc8031b422
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-05 00:48:34 +0300

    add: more numeric separators errors

--- src/data/_errors.js
+++ src/data/_errors.js
@@ -3,7 +3,7 @@ module.exports = {
 	0: "None",
 	
 	1: "Maximum call stack size exceeded appears!",
-	
+
 	200: "Can't parse array in array.",
 	201: "Can't parse object in array.",
 	
@@ -16,6 +16,11 @@ module.exports = {
 	
 	400: "Wrong command parser.",
 
-	500: "parseBigInt SyntaxError: numeric separators '_' are not allowed in numbers that start with '0'",
-	
+	500: "SyntaxError: numeric separators '_' are not allowed in numbers that start with '0'.",
+	501: "SyntaxError: numeric separators '_' cannot be used at the beginning or end.",
+	502: "SyntaxError: multiple numeric separators '_' cannot be included one after the other.",
+	503: "SyntaxError: underscore can appear only between digits, not after the last digit in a number.",
+	504: "SyntaxError: identifier starts immediately after numeric literal.",
+	505: "SyntaxError: missing exponent.",
+
 };
