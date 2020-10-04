9bdddbe2ea81ee29c164f4ee8afd3cd46a79771e
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-04 18:24:52 +0300

    add: removeUnderscores argument to support numeric separators

--- dependencies/isNumeric.js
+++ dependencies/isNumeric.js
@@ -1,11 +1,19 @@
 /*
 
-	taken from jQuery: https://jquery.com/
+	taken from jQuery: https://jquery.com/ && modified by Cameron Osakiski
 
 */
 
-function isNumeric(obj) {
+function isNumeric(obj, removeUnderscores) {
+
+	if(removeUnderscores) {
+
+		obj = obj.replace(/_/g, "");
+
+	}
+
 	return !isNaN( parseFloat(obj) ) && isFinite( obj );
+
 }
 
 module.exports = isNumeric;
