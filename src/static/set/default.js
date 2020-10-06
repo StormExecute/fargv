30334bca3b6d39a4aa2f824070fdb64b65cb99d2
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-07 00:49:28 +0300

    add: reset && redefine functionality && new save format

--- src/static/set/default.js
+++ src/static/set/default.js
@@ -1,32 +1,48 @@
 const isObject = require("../../../dependencies/isObject");
+const isEmptyObject = require("../../../dependencies/isEmptyObject");
 
 const copyV = require("../../../dependencies/copyValWithoutBind");
-
-const toJson = require("../../../dependencies/toJson");
-
 const getValue = require("../../../dependencies/getAnyPropValIfExists");
+
+//to avoid a conflict with the value that can be passed by the user
+const sign = "$fargvSetDefault";
 
 /*
 
 Format:
 
-	{
+	objectOfValues: plainObject = {
 	
-		...[name]: value | {
+		...[name]: value<any> | {
 			
-			value | argValue | v = any
+			_options: { //for no conflict if value is object
 			
-			alias | a = string | array
+				value | argValue | v = any
+				
+				alias | a | aliases = string | array
+				
+			}
 			
-		}
+		} | ["$fargvSetDefault", value<any>, alias<string | array>]
 	
-	}
+	} | "reset"
 	
 */
 
-const staticSetDefaultArgValues = function(objectOfValues) {
+//plainObject | string, boolean
+const staticSetDefaultArgValues = function(objectOfValues, redefine) {
 	
-	if(!isObject(objectOfValues) || toJson(objectOfValues) == "{}") return this;
+	if(objectOfValues == "reset" || redefine) {
+		
+		this.createOptions();
+	
+		this._options.defaultArgv = null;
+		
+		if(objectOfValues == "reset") return this;
+	
+	}
+	
+	if(!isObject(objectOfValues) || isEmptyObject(objectOfValues)) return this;
 	
 	const defaultArgv = {};
 	
@@ -38,29 +54,61 @@ const staticSetDefaultArgValues = function(objectOfValues) {
 			
 			argValue = argValue._options;
 			
-			const value = getValue(argValue, ["value", "v", "argValue"]);
+			const $sign = sign + Math.random();
 			
-			const alias = getValue(argValue, ["alias", "a", "aliases"]);
+			const value = getValue(argValue, ["value", "v", "argValue"], $sign);
+			
+			if(value == $sign) continue;
+			
+			const alias = getValue(argValue, ["alias", "a", "aliases"], $sign);
+
+			if(alias == $sign) continue;
+			
+			defaultArgv[argName] = [value, []];
 			
 			if(Array.isArray(alias)) {
 				
-				for(let i = 0; i < alias.length; i++) {
+				for(let i = 0; i < alias.length; ++i) {
 					
-					defaultArgv[alias[i]] = value;
+					if(typeof alias[i] == "string") {
+						
+						defaultArgv[argName][1].push(alias[i]);
+						
+					}
 					
 				}
 				
 			} else if(typeof alias == "string") {
 				
-				defaultArgv[alias] = value;
+				defaultArgv[argName][1] = [alias];
 				
 			}
 			
-			argValue = value;
+		} else if(Array.isArray(argValue) && argValue[0] == sign) {
+			
+			if(argValue.length > 3 || argValue.length < 2) continue;
+
+			if(argValue.length == 2) {
+
+				defaultArgv[argName] = [copyV(argValue[1]), []];
+
+			} else {
+
+				if(!Array.isArray(argValue[2]) && typeof argValue[2] != "string") continue;
+
+				if(typeof argValue[2] == "string") argValue[2] = [argValue[2]];
+
+				argValue.splice(0, 1);
+
+				defaultArgv[argName] = copyV(argValue);
+
+			}
+			
+		} else {
+		
+			defaultArgv[argName] = [copyV(argValue), []];
 			
 		}
-		
-		defaultArgv[argName] = argValue;
 		
 	}
 	
