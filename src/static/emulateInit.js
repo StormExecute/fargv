a696079cfe0b853e96ee5c017b6935991b8b38bf
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-07 23:38:32 +0300

    up: save merged options in fargvWrapper instead of running without saving

--- src/static/emulateInit.js
+++ src/static/emulateInit.js
@@ -7,8 +7,12 @@ const init = function(options, mergingWithFargvWrapperOptions) {
 	if(!mergingWithFargvWrapperOptions) return this(options);
 	
 	if(!isObject(options)) return this();
+
+	this.createOptions();
+
+	this._options = deepCloneObject(this._options, options);
 	
-	return this(deepCloneObject({}, this._options, options));
+	return this();
 	
 };
 
