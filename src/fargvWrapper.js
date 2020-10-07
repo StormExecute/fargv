e2a3440ac5dec79cbc2a5ad2ec891c18cb9110f4
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-07 13:20:23 +0300

    add: real abstract methods to avoid conflicts with the IDE

--- src/fargvWrapper.js
+++ src/fargvWrapper.js
@@ -1,60 +1,73 @@
 const isObject = require("../dependencies/isObject");
 
-const staticMethods = require("./static");
+const staticMethods = require("./static/index");
 
-/*
+let abstractStaticMethods = {
 
-	abstractALLFargvWrapperProperties:
+	//abstractALLFargvWrapperProperties:
 
-		options,
-		
-		command,
-		
-		"default": _default,
-		custom,
-		customWithGenerate,
-		customWithGenerateFromObject,
-		
-		demand,
-		undemand,
-		
-		exclude,
-		
-		noParse,
-		noParseFlags,
-		
-		allParse,
-		
-		remember,
-		
-		warns,
-		
-		arrayParse,
-		objectParse,
-		
-		state,
-		
-		fromArray: toFargvStringArray,
-		fromObject: toFargvStringObject,
-		
-		toArray: fromFargvStringArray,
-		toObject: fromFargvStringObject,
-		
-		toFargvStringArray,
-		toFargvStringObject,
-		
-		fromFargvStringArray,
-		fromFargvStringObject,
-		
-		tryToArray,
-		tryToObject,
-		
-		generate,
-		generateFromObject,
-		
-		init,
-	
-*/
+	_options: {},
+
+	options(options, redefine){},
+
+	createOptions(){},
+
+	reset(option){},
+	resetAll(){},
+
+	command(command, handler, help){},
+
+	option(optionName, optionConfig){},
+
+	help(objectOfHelp){},
+
+	"default": (objectOfValues, redefine) => {},
+	custom(customArgv){},
+	customWithGenerate(...args){},
+	customWithGenerateFromObject(...args){},
+
+	demand(args, withSkipArgs){},
+	undemand(args, withSkipArgs){},
+
+	exclude(excludeFlags, redefine){},
+
+	noParse(state, isNoParseNoDefault){},
+	noParseFlags(noParseFlags, isNoParseNoDefaultFlags){},
+
+	allParse(allParseState){},
+
+	remember(rememberState){},
+
+	warns(optionsState){},
+
+	arrayParse(arrayParse, redefine){},
+	objectParse(objectParse, redefine){},
+
+	state(about){},
+
+	fromArray(sourceArray, useSpacesAsDelimiters){},
+	fromObject(sourceObject, useSpacesAsDelimiters){},
+
+	toArray(sourceString, mergingOptions){},
+	toObject(sourceString, mergingOptions){},
+
+	toFargvStringArray(sourceArray, useSpacesAsDelimiters){},
+	toFargvStringObject(sourceObject, useSpacesAsDelimiters){},
+
+	fromFargvStringArray(sourceString, mergingOptions){},
+	fromFargvStringObject(sourceString, mergingOptions){},
+
+	tryToArray(sourceString, mergingOptions){},
+	tryToObject(sourceString, mergingOptions){},
+
+	generate(argName, argValue, ...args){},
+	generateFromObject(getParsedArgs, objectOfValues, objectOfConfigs){},
+
+	init(options, mergingWithFargvWrapperOptions){},
+
+}
+
+abstractStaticMethods = Object.assign(abstractStaticMethods, staticMethods);
 
 /*
 
@@ -94,7 +107,7 @@ function fargvWrapper(options) {
 	
 }
 
-fargvWrapper = Object.assign(fargvWrapper, staticMethods);
+fargvWrapper = Object.assign(fargvWrapper, abstractStaticMethods);
 
 module.exports = {
 	
