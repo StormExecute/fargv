ba9884d44629696cb60c7666f1ce972a917c2bf6
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-10 22:41:54 +0300

    add: support for new command options 1 - separateCommandHandler 2 - callAppropriateCommandHandlerOnlyOnce 3 - alwaysCallSeparateCommandHandler

--- src/parseCommands.js
+++ src/parseCommands.js
@@ -2,35 +2,74 @@ const cutByInitialMatch = require("../dependencies/cutByInitialMatch");
 
 const includeCommand = require("./includeCommand");
 
+const callHandler = (nextCommandsAsArray, handler, state, nextCommands) => {
+
+	if(nextCommandsAsArray) {
+
+		handler(state, nextCommands);
+
+	} else {
+
+		handler(state, ...nextCommands);
+
+	}
+
+	return null;
+
+};
+
+const makeState = parsedArgs => {
+
+	const copyOfParsedArgs = Object.assign({}, parsedArgs);
+
+	const state = {};
+
+	if(copyOfParsedArgs._) {
+
+		state._ = copyOfParsedArgs._;
+
+		delete copyOfParsedArgs._;
+
+	}
+
+	if(copyOfParsedArgs._warns) {
+
+		state.warns = copyOfParsedArgs._warns;
+
+		delete copyOfParsedArgs._warns;
+
+	}
+
+	state.flags = copyOfParsedArgs;
+
+	return state;
+
+};
+
+const callSeparateCommandHandler = ({ nextCommandsAsArray, separateCommandHandler }, state, rememberCommands) => {
+
+	callHandler(
+		nextCommandsAsArray,
+		separateCommandHandler,
+		state,
+		rememberCommands
+	);
+
+	return null;
+
+};
+
 const fargvParseCommandArgs = function (rememberCommands, parsedArgs) {
 	
 	if(!this.validateCommands()) return;
-	
+
 	this.usableOptions.commands = this.usableOptions.commands.sort((a, b) => b[0].split(" ").length - a[0].split(" ").length);
 
-	const copyOfParsedArgs = Object.assign({}, parsedArgs);
-	
-	const state = {};
-	
-	if(copyOfParsedArgs._) {
-		
-		state._ = copyOfParsedArgs._;
-		
-		delete copyOfParsedArgs._;
-		
-	}
-	
-	if(copyOfParsedArgs._warns) {
-		
-		state.warns = copyOfParsedArgs._warns;
-		
-		delete copyOfParsedArgs._warns;
-		
-	}
-	
-	state.flags = copyOfParsedArgs;
+	const state = makeState(parsedArgs);
 	
 	const argvCommandsToStr = rememberCommands.join(" ");
+
+	let foundAppropriateCommand = false;
 	
 	for(let i = 0; i < this.usableOptions.commands.length; i++) {
 		
@@ -39,27 +78,40 @@ const fargvParseCommandArgs = function (rememberCommands, parsedArgs) {
 		const [command, handler] = this.usableOptions.commands[i];
 		
 		if(includeCommand(argvCommandsToStrThis, command)) {
+
+			if(!foundAppropriateCommand) foundAppropriateCommand = true;
 			
 			argvCommandsToStrThis = cutByInitialMatch(argvCommandsToStrThis, command);
 			
 			if(argvCommandsToStrThis[0] == " ") argvCommandsToStrThis = argvCommandsToStrThis.slice(1);
 			
 			const nextCommands = argvCommandsToStrThis.split(" ");
-			
-			if(this.usableOptions.nextCommandsAsArray) {
-				
-				handler(state, nextCommands);
-				
-			} else {
-				
-				handler(state, ...nextCommands);
-				
-			}
+
+			callHandler(this.usableOptions.nextCommandsAsArray, handler, state, nextCommands);
+
+			//break if we found appropriate command and callAppropriateCommandHandlerOnlyOnce is true
+			if(this.usableOptions.callAppropriateCommandHandlerOnlyOnce) break;
 			
 		}
 		
 	}
+
+	if(
+		typeof this.usableOptions.separateCommandHandler == "function" && (
+			this.usableOptions.alwaysCallSeparateCommandHanler ||
+			!foundAppropriateCommand
+		)
+	) {
+
+		callSeparateCommandHandler(this.usableOptions, state, rememberCommands);
+
+	}
+
+	return null;
 	
 };
+
+fargvParseCommandArgs.makeState = makeState;
+fargvParseCommandArgs.callSeparateCommandHandler = callSeparateCommandHandler;
 
 module.exports = fargvParseCommandArgs;
