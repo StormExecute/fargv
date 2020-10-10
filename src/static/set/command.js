261e2345853d996c5735bbd4bd94cfab72a509e6
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-10 23:09:52 +0300

    add: setSeparateCommandHandler support && more overloads

--- src/static/set/command.js
+++ src/static/set/command.js
@@ -4,27 +4,46 @@ const isObject = require("../../../dependencies/isObject");
 
 	Format:
 	
-		fargv.command("command" | "command thenCommand", function(state, thenCommands | ...thenCommands) {
-			
-			state = {
+		fargv.command(
+		
+			"command" | "command thenCommand" | array ["command", "commandAlias", ...],
+
+			function(state, thenCommands | ...thenCommands) {
 				
-				flags: {},
-				
-				_: {},
-				
-				warns: {}
+				state = {
+					
+					flags: {},
+					
+					_: {},
+					
+					warns: {}
+					
+				}
 				
 			}
-			
-		})
+
+		)
 
 */
 
 const staticSetCommand = function(command, handler) {
+
+	//setSeparateCommandHandler
+	if(typeof command == "function") {
+
+		this.createOptions();
+
+		this._options.separateCommandHandler = command;
+
+		return this;
+
+	}
+
+	const commandIsArray = Array.isArray(command);
 	
-	if(typeof command != "string" || typeof handler != "function") return this;
+	if((typeof command != "string" && !commandIsArray) || typeof handler != "function") return this;
 	
-	if(!isObject(this._options)) this._options = {};
+	this.createOptions();
 	
 	if(command == "reset") {
 	
@@ -34,16 +53,31 @@ const staticSetCommand = function(command, handler) {
 	
 	}
 	
-	const commands = [command, handler];
+	const newCommand = commandIsArray ? [] : [[command, handler]];
+
+	if(commandIsArray) {
+
+		for(let i = 0; i < command.length; ++i) {
+
+			if(typeof command[i] == "string") {
+
+				newCommand.push([command[i], handler]);
+
+			}
+
+		}
+
+		//here
+		if(!newCommand.length) return this;
+
+	}
 	
-	if(!Array.isArray(this._options.commands)) {
-		
-		this._options.commands = [commands];
-		
-	} else {
-		
-		this._options.commands.push(commands);
-		
+	if(!Array.isArray(this._options.commands)) this._options.commands = [];
+
+	for (let i = 0; i < newCommand.length; ++i) {
+
+		this._options.commands.push(newCommand[i]);
+
 	}
 	
 	return this;
