a79f4b985cb878921ee70a635b22f5c92349fdb3
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-13 02:03:56 +0300

    add: initF, initC, initFC to init with fast set returnFilter

--- src/static/emulateInit.js
+++ src/static/emulateInit.js
@@ -5,7 +5,63 @@ const { deepCloneObject } = require("../../dependencies/deepClone");
 const init = function() {
 
 	return this.apply(global, arguments);
-	
+
 };
 
-module.exports = init;
+const initF = function (options) {
+
+	if(isObject(options)) {
+
+		options.returnFilter = "flags";
+
+	} else {
+
+		this.returnFilter("flags");
+
+	}
+
+	return this.init.apply(this, arguments);
+
+};
+
+const initC = function (options) {
+
+	if(isObject(options)) {
+
+		options.returnFilter = "commands";
+
+	} else {
+
+		this.returnFilter("commands");
+
+	}
+
+	return this.init.apply(this, arguments);
+
+};
+
+const initFC = function (options) {
+
+	if(isObject(options)) {
+
+		options.returnFilter = ["flags", "commands"];
+
+	} else {
+
+		this.returnFilter(["flags", "commands"]);
+
+	}
+
+	return this.init.apply(this, arguments);
+
+};
+
+module.exports = {
+
+	init,
+
+	initF,
+	initC,
+	initFC,
+
+};
