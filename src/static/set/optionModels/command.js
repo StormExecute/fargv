e7b7d96ed6ded6e95b5440d885a2705b566d7261
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-17 01:10:23 +0300

    add: option default models to set help props

---
+++ src/static/set/optionModels/command.js
@@ -0,0 +1,13 @@
+const optionCommandDefaultModel = {
+
+	alias: null,
+
+	desc: null,
+	usage: null,
+
+	flags: null,
+	examples: null,
+
+};
+
+module.exports = optionCommandDefaultModel;
---
+++ src/static/set/optionModels/flag.js
@@ -0,0 +1,20 @@
+const optionFlagDefaultModel = {
+	
+	desc: null,
+	
+	required: false,
+	deprecated: false,
+
+	//if null - don't show any
+	empty: null,
+	
+	type: null,
+	
+	default: null,
+	alias: null,
+	
+	examples: null,
+	
+};
+
+module.exports = optionFlagDefaultModel;
